5187bde50ceb1929569edf470d456b2301e9c47c
KaweVK <victorkawe07@gmail.com>
2025-11-20 19:39:38 -0300

    Adicionando rotas

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Login } from './pages/Login/index.jsx'
 import { CadastroProduto } from './pages/CadastroProduto/index.jsx'
+import { BrowserRouter, Route, Routes } from 'react-router'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <CadastroProduto />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/cadastro-produto" element={<CadastroProduto />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
