67359aab633fcd9448606b6579bf85e296103099
KaweVK <victorkawe07@gmail.com>
2025-11-20 19:34:26 -0300

    Formulario de login

---
+++ src/componentes/FormularioLogin/index.jsx
@@ -0,0 +1,44 @@
+import './FormularioLogin.css'
+import CampoTexto from '../CampoTexto'
+import Botao from '../Botao'
+import { useState } from 'react'
+
+export const FormularioLogin = (props) => {
+
+    const [email, setEmail] = useState('') 
+    const [senha, setSenha] = useState('') 
+
+    const aoSalvar = (evento) => {
+        evento.preventDefault()
+        props.aoCadastrarProduto({
+            email,
+            senha
+        })
+        setEmail('')
+        setSenha('')
+    }
+
+    return (
+        <section className='formulario'>
+            <form onSubmit={aoSalvar}>
+                <h2>Login</h2>
+                <CampoTexto 
+                    obrigatorio={true} 
+                    label='Email' 
+                    placeholder='Digite o email do login' 
+                    valor={email}
+                    aoAlterado={valor => setEmail(valor)}/>
+                <CampoTexto 
+                    obrigatorio={true} 
+                    label='Senha' 
+                    placeholder='Digite a sua senha'
+                    valor={senha}
+                    aoAlterado={valor => setSenha(valor)}/>
+                <Botao>
+                    Cadastrar
+                </Botao>
+            </form>
+        </section>
+    )
+
+}
