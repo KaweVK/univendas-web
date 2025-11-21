fab0f0fa149bd80c5e498014c065c9ddcd09c531
KaweVK <victorkawe07@gmail.com>
2025-11-21 15:32:10 -0300

    mudança na visualização

--- src/componentes/DetalheUsuario/index.jsx
+++ src/componentes/DetalheUsuario/index.jsx
@@ -1,12 +1,12 @@
 import './DetalheUsuario.css';
 
-export const DetalheUsuario = ({ id, nome, email, telefone, img, cidade }) => {
+export const DetalheUsuario = ({ nome, email, telefone, img, cidade }) => {
     return (
         <>
             <div className='detalhe-usuario'>
-                <h1>Perfil do Usuário {id}</h1>
+                <h1>Perfil do Usuário</h1>
                 <img src={img} alt={`Foto de ${nome}`} />
-                <h2>{nome}</h2>
+                <h2>Nome: {nome}</h2>
                 <p>Email: {email}</p>
                 <p>Telefone: {telefone}</p>
                 <p>Cidade: {cidade}</p>
