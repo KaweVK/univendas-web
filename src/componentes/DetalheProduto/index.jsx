ebd2d46b117b39ab041a1ce75d40f42ec730b0ff
KaweVK <victorkawe07@gmail.com>
2025-11-21 00:22:34 -0300

    Id removido

--- src/componentes/DetalheProduto/index.jsx
+++ src/componentes/DetalheProduto/index.jsx
@@ -1,11 +1,10 @@
 import './DetalheProduto.css';
 
-export const DetalheProduto = ({id, nome, descricao, preco, img }) => {
+export const DetalheProduto = ({nome, descricao, preco, img }) => {
     return (
         <div className='detalhe-produto'>
-            <h1>Produto {id}</h1>
+            <h1>{nome}</h1>
             <img src={img} alt={nome} />
-            <h2>{nome}</h2>
             <p>{descricao}</p>
             <h3>{preco}</h3>
         </div>
