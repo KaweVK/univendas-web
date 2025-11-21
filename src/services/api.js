7a2bfe789bda2a2f7474c2071d1f56ac7f6a19b4
KaweVK <victorkawe07@gmail.com>
2025-11-21 16:06:00 -0300

    Update para resolver erro do token

--- src/services/api.js
+++ src/services/api.js
@@ -16,4 +16,21 @@ api.interceptors.request.use((config) => {
     return Promise.reject(error);
 });
 
+api.interceptors.response.use(
+    (response) => {
+        return response;
+    },
+    (error) => {
+        if (error.response && (error.response.status === 403 || error.response.status === 401)) {
+            localStorage.removeItem('token');
+            
+            if (window.location.pathname !== '/auth/login') {
+                window.location.href = '/auth/login';
+                alert("Sua sessão expirou. Faça login novamente.");
+            }
+        }
+        return Promise.reject(error);
+    }
+);
+
 export default api;
