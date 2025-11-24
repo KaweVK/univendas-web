cadd9d04d81306d3a2a6664184b0d9ebd693e28b
KaweVK <victorkawe07@gmail.com>
2025-11-24 15:04:14 -0300

    Nova navbar

--- src/componentes/NavBar/index.jsx
+++ src/componentes/NavBar/index.jsx
@@ -1,9 +1,49 @@
-import './NavBar.css'
-import { Link } from 'react-router'
-import { useNavigate } from 'react-router'
+import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import "./NavBar.css"
+import { useState, useEffect } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
+import api from '../../services/api.js';
+import { jwtDecode } from 'jwt-decode';
+
+
+function classNames(...classes) {
+    return classes.filter(Boolean).join(' ')
+}
 
 export const NavBar = () => {
+    const location = useLocation()
+
+    const token = localStorage.getItem('token');
+    let id = null;
+
+    if (token) {
+        const decoded = jwtDecode(token);
+        id = decoded.id;
+    }
+
     const navigate = useNavigate();
+    const [usuario, setUsuario] = useState(null);
+
+    useEffect(() => {
+        const carregarUsuario = async () => {
+            try {
+                const resposta = await api.get(`/users/${id}`);
+                setUsuario(resposta.data);
+            } catch (erro) {
+                console.error("Erro ao buscar usuário:", erro);
+                alert("Erro ao carregar perfil.");
+                navigate('/usuarios');
+            }
+        }
+        carregarUsuario();
+    }, [id, navigate]);
+
+
+    const navigation = [
+        { name: 'Produtos', href: '/produtos', current: location.pathname === '/produtos' },
+        { name: 'Usuários', href: '/usuarios', current: location.pathname === '/usuarios' },
+    ]
 
     const realizarLogout = () => {
         window.confirm("Deseja realmente sair?")
@@ -11,26 +51,110 @@ export const NavBar = () => {
         navigate('/auth/login');
     }
 
-    return (<footer className="bar">
-        <section>
-            <ul>
-                <li>
-                    <Link to={`/produtos`} style={{ color: "#FFF", marginLeft:"-10pxpx" }}>
-                        Produtos
-                    </Link>
-                </li>
-                <li>
-                    <Link to={`/usuarios`} style={{ color: "#FFF", marginLeft:"-10pxpx" }}>
-                        Usuarios
-                    </Link>
-                </li>
-            </ul>
-        </section>
-        <section>
-            <Link onClick={realizarLogout} to={"/auth/login"} style={{ color: "#FFF"}}>
-                Logout
-            </Link>
-        </section>
-    </footer>
+    const perfil = () => {
+        navigate(`/usuario/${usuario.id}`)
+    }
+
+    return (
+        <Disclosure
+            as="nav"
+            className="relative nav-color after:pointer-events-none after:absolute after:inset-x-0 after:bottom-0 after:h-px after:bg-white/10"
+        >
+            <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+                <div className="relative flex h-16 items-center justify-between">
+                    <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                        {/* Mobile menu button*/}
+                        <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-white/5 hover:text-white focus:outline-2 focus:-outline-offset-1 focus:outline-indigo-500">
+                            <span className="absolute -inset-0.5" />
+                            <span className="sr-only">Menu</span>
+                            <Bars3Icon aria-hidden="true" className="block size-6 group-data-open:hidden" />
+                            <XMarkIcon aria-hidden="true" className="hidden size-6 group-data-open:block" />
+                        </DisclosureButton>
+                    </div>
+                    <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                        <div className="flex shrink-0 items-center">
+                            <img
+                                alt="Univendas logo"
+                                src="/imagens/Logos/univendas.png"
+                                className="h-8 w-auto"
+                            />
+                        </div>
+                        <div className="hidden sm:ml-6 sm:block">
+                            <div className="flex space-x-4">
+                                {navigation.map((item) => (
+                                    <a
+                                        key={item.name}
+                                        href={item.href}
+                                        aria-current={item.current ? 'page' : undefined}
+                                        className={classNames(
+                                            item.current ? 'bg-gray-950/50 text-white' : 'text-gray-300 hover:bg-white/5 hover:text-white',
+                                            'rounded-md px-3 py-2 text-sm font-medium',
+                                        )}
+                                    >
+                                        {item.name}
+                                    </a>
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+                    <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+
+                        {/* Profile dropdown */}
+                        <Menu as="div" className="relative ml-3">
+                            <MenuButton className="relative flex rounded-full focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">
+                                <span className="absolute -inset-1.5" />
+                                <span className="sr-only">Open user menu</span>
+                                <img
+                                    alt=""
+                                    src={usuario?.imageUrl}
+                                    className="size-8 rounded-full bg-gray-800 outline -outline-offset-1 outline-white/10"
+                                />
+                            </MenuButton>
+
+                            <MenuItems
+                                transition
+                                className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-gray-800 py-1 outline -outline-offset-1 outline-white/10 transition data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in"
+                            >
+                                <MenuItem>
+                                    <a
+                                        onClick={perfil}
+                                        className="block px-4 py-2 text-sm text-gray-300 data-focus:bg-white/5 data-focus:outline-hidden"
+                                    >
+                                        Perfil
+                                    </a>
+                                </MenuItem>
+                                <MenuItem>
+                                    <a
+                                        onClick={realizarLogout}
+                                        className="block px-4 py-2 text-sm text-gray-300 data-focus:bg-white/5 data-focus:outline-hidden"
+                                    >
+                                        Sign out
+                                    </a>
+                                </MenuItem>
+                            </MenuItems>
+                        </Menu>
+                    </div>
+                </div>
+            </div>
+
+            <DisclosurePanel className="sm:hidden">
+                <div className="space-y-1 px-2 pt-2 pb-3">
+                    {navigation.map((item) => (
+                        <DisclosureButton
+                            key={item.name}
+                            as="a"
+                            href={item.href}
+                            aria-current={item.current ? 'page' : undefined}
+                            className={classNames(
+                                item.current ? 'bg-gray-950/50 text-white' : 'text-gray-300 hover:bg-white/5 hover:text-white',
+                                'block rounded-md px-3 py-2 text-base font-medium',
+                            )}
+                        >
+                            {item.name}
+                        </DisclosureButton>
+                    ))}
+                </div>
+            </DisclosurePanel>
+        </Disclosure>
     )
 }
