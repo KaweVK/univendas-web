c4cf8018a38e92bc8d86acb3562a32365d836837
KaweVK <victorkawe07@gmail.com>
2025-11-24 19:14:03 -0300

    Fix

--- src/componentes/Formulario/index.jsx
+++ src/componentes/Formulario/index.jsx
@@ -1,19 +1,19 @@
 import './Formulario.css'
-import {CampoTexto} from '../CampoTexto'
-import {ListaSuspensa} from '../ListaSuspensa'
-import {Botao} from '../Botao'
-import { useState, useEffect } from 'react' 
+import { CampoTexto } from '../CampoTexto'
+import { ListaSuspensa } from '../ListaSuspensa'
+import { Botao } from '../Botao'
+import { useState, useEffect } from 'react'
 
 export const Formulario = (props) => {
 
-    const [nome, setNome] = useState('') 
-    const [descricao, setDescricao] = useState('') 
-    const [quantidade, setQuantidade] = useState('') 
-    const [preco, setPreco] = useState('') 
+    const [nome, setNome] = useState('')
+    const [descricao, setDescricao] = useState('')
+    const [quantidade, setQuantidade] = useState('')
+    const [preco, setPreco] = useState('')
     const [categoria, setCategoria] = useState('')
-    const [imagem, setImagem] = useState('') 
+    const [imagem, setImagem] = useState('')
 
-  
+
     useEffect(() => {
         if (props.produtoEdicao) {
             setNome(props.produtoEdicao.name || '');
@@ -49,46 +49,46 @@ export const Formulario = (props) => {
         <section className='formulario'>
             <form onSubmit={aoSalvar}>
                 <h2>{props.produtoEdicao ? 'Editar Produto' : 'Preencha os dados para criar seu produto'}</h2>
-                
-                <CampoTexto 
-                    obrigatorio={true} 
-                    label='Nome' 
-                    placeholder='Digite o nome do produto' 
+
+                <CampoTexto
+                    obrigatorio={true}
+                    label='Nome'
+                    placeholder='Digite o nome do produto'
                     valor={nome}
-                    aoAlterado={valor => setNome(valor)}/>
-                
-                
-                <CampoTexto 
-                    obrigatorio={true} 
-                    label='Descrição' 
+                    aoAlterado={valor => setNome(valor)} />
+
+
+                <CampoTexto
+                    obrigatorio={true}
+                    label='Descrição'
                     placeholder='Digite a descrição do produto'
                     valor={descricao}
-                    aoAlterado={valor => setDescricao(valor)}/>
+                    aoAlterado={valor => setDescricao(valor)} />
                 <CampoTexto
-                    obrigatorio={true} 
-                    label='Quantidade' 
+                    obrigatorio={true}
+                    label='Quantidade'
                     placeholder='Digite quantidade do produto'
                     valor={quantidade}
-                    aoAlterado={valor => setQuantidade(valor)}/>
-                <CampoTexto 
-                    obrigatorio={true} 
-                    label='Preço' 
+                    aoAlterado={valor => setQuantidade(valor)} />
+                <CampoTexto
+                    obrigatorio={true}
+                    label='Preço'
                     placeholder='Digite o preço do produto'
                     valor={preco}
-                    aoAlterado={valor => setPreco(valor)}/>
-                <CampoTexto 
-                    obrigatorio={true} 
-                    label='Imagem' 
+                    aoAlterado={valor => setPreco(valor)} />
+                <CampoTexto
+                    obrigatorio={true}
+                    label='Imagem'
                     placeholder='Digite o link da imagem do produto'
                     valor={imagem}
-                    aoAlterado={valor => setImagem(valor)}/>
-                <ListaSuspensa 
-                    obrigatorio={true} 
-                    itens={props.categorias} 
+                    aoAlterado={valor => setImagem(valor)} />
+                <ListaSuspensa
+                    obrigatorio={true}
+                    itens={props.categorias}
                     label='Categoria'
                     valor={categoria}
-                    aoAlterado={valor => setCategoria(valor)}/>
-                
+                    aoAlterado={valor => setCategoria(valor)} />
+
                 <Botao className={'botao-padrao'}>
                     {props.produtoEdicao ? 'Salvar Alterações' : 'Cadastrar'}
                 </Botao>
