acb54f0cd1b3bd5e31d8fd2dc26bb91359167d2f
KaweVK <victorkawe07@gmail.com>
2025-11-26 21:41:05 -0300

    Ajuste na exibição

--- src/componentes/DetalheUsuario/index.jsx
+++ src/componentes/DetalheUsuario/index.jsx
@@ -6,10 +6,10 @@ export const DetalheUsuario = ({ nome, email, telefone, img, cidade }) => {
             <div className='detalhe-usuario'>
                 <h1>Perfil do Usuário</h1>
                 <img src={img} alt={`Foto de ${nome}`} />
-                <h2>Nome: {nome}</h2>
-                <p>Email: {email}</p>
-                <p>Telefone: {telefone}</p>
-                <p>Cidade: {cidade}</p>
+                <h2><strong>Nome:</strong> {nome}</h2>
+                <p><strong>Email:</strong> {email}</p>
+                <p><strong>Telefone:</strong> {telefone}</p>
+                <p><strong>Cidade:</strong> {cidade}</p>
             </div>
         </>
     )
